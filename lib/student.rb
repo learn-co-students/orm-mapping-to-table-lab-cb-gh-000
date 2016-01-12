ea851bd27531975fdaca44d120322a1ee8d3898e
Avi Flombaum <avi@flombaum.com>
2016-01-12 10:51:40 -0500

    Remove require environment and pry from classes

--- lib/student.rb
+++ lib/student.rb
@@ -1,11 +1,6 @@
-require 'pry'
-require_relative "../config/environment.rb"
-
 class Student
 
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]  
   
-  
-
 end
