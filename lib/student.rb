15ef1e4f7aef2e6d5838e3e27da188c08244b2ea
robertjosephwayne <robertjwayne@gmail.com>
2017-04-22 19:37:51 -0400

    Initialize Student with name and grade

--- lib/student.rb
+++ lib/student.rb
@@ -1,6 +1,12 @@
 class Student
+  attr_reader :id
 
   # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]  
+  #  with DB[:conn]
+
+  def initialize(name, grade)
+    @name = name
+    @grade = grade
+  end
   
 end
